024a84789716be4ab9e8f8d4d50309bcc3b10589
SUPATKN <yacht1400@gmail.com>
2024-09-01 12:31:24 +0700

    แก้ไฟล์ migrate

--- my-app/db/schema.ts
+++ my-app/db/schema.ts
@@ -10,14 +10,14 @@ import {
 } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
-  id: integer("id").primaryKey(),
+  id: serial("id").primaryKey(),
   username: varchar("username", { length: 50 }).notNull(),
   email: varchar("email", { length: 100 }).unique().notNull(),
   password: text("password").notNull(),
 });
 
 export const images = pgTable("images", {
-  id: integer("id").primaryKey(),
+  id: serial("id").primaryKey(),
   path: text("path"),
   user_id: integer("user_id")
     .notNull()
@@ -29,7 +29,7 @@ export const images = pgTable("images", {
 });
 
 export const ProfilePicture = pgTable("ProfilePicture", {
-  id: integer("id").primaryKey(),
+  id: serial("id").primaryKey(),
   path: text("path"),
   user_id: integer("user_id")
     .notNull()
@@ -61,7 +61,7 @@ export const likes = pgTable(
 );
 
 export const comments = pgTable("comments", {
-  comment_id: integer("comment_id").primaryKey(),
+  comment_id: serial("comment_id").primaryKey(),
   photo_id: integer("photo_id")
     .notNull()
     .references(() => images.id, {
