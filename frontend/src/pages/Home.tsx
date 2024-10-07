22573708a74fef54cd2c9c8c2f56cc8d66ef63fe
touch <tttouch1122@gmail.com>
2024-10-07 20:31:02 +0700

    edit upload in home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { Image, Row, Col, Button} from "react-bootstrap";
+import { Image, Row, Col, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUpload } from "@fortawesome/free-solid-svg-icons";
@@ -11,7 +11,6 @@ import Logo from "../LOGOArtandCommunity.png";
 import Layout from "../Layout";
 import useAuth from "../hook/useAuth";
 import { shuffle } from "lodash";
-
 
 const Home = () => {
   const [photos, setPhotos] = useState<
@@ -147,90 +146,100 @@ const Home = () => {
       console.error("Error adding photo to cart:", error);
     }
   };
-  
+
   return (
     <Layout>
       <div className="Home">
         <div className="header-home">
           <img src={Logo} alt="My Photo App Logo" className="Logo" />
           <div className="header-text">
-            <h5 className="text-center text-[#ffffff] " >W</h5>
-            <h5 className="text-center text-[#ffffff] " >E</h5>
-            <h5 className="text-center text-[#ffffff] " >L</h5>
-            <h5 className="text-center text-[#ffffff] " >C</h5>
-            <h5 className="text-center text-[#ffffff] " >O</h5>
-            <h5 className="text-center text-[#ffffff] " >M</h5>
-            <h5 className="text-center text-[#ffffff] " >E</h5>
+            <h5 className="text-center text-[#ffffff] ">W</h5>
+            <h5 className="text-center text-[#ffffff] ">E</h5>
+            <h5 className="text-center text-[#ffffff] ">L</h5>
+            <h5 className="text-center text-[#ffffff] ">C</h5>
+            <h5 className="text-center text-[#ffffff] ">O</h5>
+            <h5 className="text-center text-[#ffffff] ">M</h5>
+            <h5 className="text-center text-[#ffffff] ">E</h5>
 
-            <div className="TO"> 
-              <h5 className="text-center text-[#ffffff] " >T</h5>
-              <h5 className="text-center text-[#ffffff] " >O</h5>
-            </div>
-
-            <h5 className="text-center text-[#ff8833] BordershowY" >A</h5>
-            <h5 className="text-center text-[#ff8833] " >R</h5>
-            <h5 className="text-center text-[#ff8833] " >T</h5>
-            
             <div className="TO">
-              <h5 className="text-center text-[#ff8833] " >A</h5>
-              <h5 className="text-center text-[#ff8833] " >N</h5>
-              <h5 className="text-center text-[#ff8833] " >D</h5>
+              <h5 className="text-center text-[#ffffff] ">T</h5>
+              <h5 className="text-center text-[#ffffff] ">O</h5>
             </div>
 
-            <h5 className="text-center text-[#ff8833] Bordershow" >C</h5>
-            <h5 className="text-center text-[#ff8833] " >O</h5>
-            <h5 className="text-center text-[#ff8833] " >M</h5>
-            <h5 className="text-center text-[#ff8833] " >M</h5>
-            <h5 className="text-center text-[#ff8833] " >U</h5>
-            <h5 className="text-center text-[#ff8833] " >N</h5>
-            <h5 className="text-center text-[#ff8833] " >I</h5>
-            <h5 className="text-center text-[#ff8833] " >T</h5>
-            <h5 className="text-center text-[#ff8833] BordershowX" >Y</h5>
-        
+            <h5 className="text-center text-[#ff8833] BordershowY">A</h5>
+            <h5 className="text-center text-[#ff8833] ">R</h5>
+            <h5 className="text-center text-[#ff8833] ">T</h5>
+
+            <div className="TO">
+              <h5 className="text-center text-[#ff8833] ">A</h5>
+              <h5 className="text-center text-[#ff8833] ">N</h5>
+              <h5 className="text-center text-[#ff8833] ">D</h5>
+            </div>
+
+            <h5 className="text-center text-[#ff8833] Bordershow">C</h5>
+            <h5 className="text-center text-[#ff8833] ">O</h5>
+            <h5 className="text-center text-[#ff8833] ">M</h5>
+            <h5 className="text-center text-[#ff8833] ">M</h5>
+            <h5 className="text-center text-[#ff8833] ">U</h5>
+            <h5 className="text-center text-[#ff8833] ">N</h5>
+            <h5 className="text-center text-[#ff8833] ">I</h5>
+            <h5 className="text-center text-[#ff8833] ">T</h5>
+            <h5 className="text-center text-[#ff8833] BordershowX">Y</h5>
           </div>
         </div>
         <div className="description-home">
-          <h5 className="text-center text-[#ffffff] ">A website that will make your</h5>
-          <h5 className="text-center  text-[#ff8833]" >  “photos”</h5>
-          <h5 className="display-flex text-center text-[#ffffff] ">more valuable than they are.</h5>
+          <h5 className="text-center text-[#ffffff] ">
+            A website that will make your
+          </h5>
+          <h5 className="text-center  text-[#ff8833]"> “photos”</h5>
+          <h5 className="display-flex text-center text-[#ffffff] ">
+            more valuable than they are.
+          </h5>
         </div>
       </div>
 
-      <Link to="/login" className={`start-uploadHome ${location.pathname === '' ? 'active' : ''}`}>
-              <FontAwesomeIcon icon={faUpload}/>  
-              <div className={`pl-2${location.pathname === '' ? 'active' : ''}`} > You can now upload your photos </div>
+      <Link
+        to="/upload"
+        className={`start-uploadHome ${
+          location.pathname === "" ? "active" : ""
+        }`}
+      >
+        <FontAwesomeIcon icon={faUpload} />
+        <div className={`pl-2${location.pathname === "" ? "active" : ""}`}>
+          {" "}
+          You can now upload your photos{" "}
+        </div>
       </Link>
 
       <div className="photo-container">
         <div className="photo-box">
           <div className="photo-box-inner">
-          <Row className="rowpic">
-              {shuffle(photos.slice()).slice(0, 4).map((photo) =>(
-                <Col key={photo.id} xs={12} md={4} lg={3} className="mb-2">
-                  <div
-                    className="pict relative"
-                  >
-                    <div className="relative flex flex-col items-center justify-center">
-                      <Image
-                        crossOrigin="anonymous"
-                        src={`/api/${photo.path}`}
-                        alt={`Image ${photo.id}`}
-                        className="w-full h-fit rounded-md cursor-pointer"
-                        onClick={() => handlePhotoClick(photo.id)}
-                        onContextMenu={(e) => handlePhotoContextMenu(e, photo.id)}
-                      />
+            <Row className="rowpic">
+              {shuffle(photos.slice())
+                .slice(0, 4)
+                .map((photo) => (
+                  <Col key={photo.id} xs={12} md={4} lg={3} className="mb-2">
+                    <div className="pict relative">
+                      <div className="relative flex flex-col items-center justify-center">
+                        <Image
+                          crossOrigin="anonymous"
+                          src={`/api/${photo.path}`}
+                          alt={`Image ${photo.id}`}
+                          className="w-full h-fit rounded-md cursor-pointer"
+                          onClick={() => handlePhotoClick(photo.id)}
+                          onContextMenu={(e) =>
+                            handlePhotoContextMenu(e, photo.id)
+                          }
+                        />
+                      </div>
                     </div>
-                  </div>
-                </Col>
-              ))}
+                  </Col>
+                ))}
             </Row>
           </div>
         </div>
       </div>
-     
-    </Layout> 
-
-    
+    </Layout>
   );
 };
 
