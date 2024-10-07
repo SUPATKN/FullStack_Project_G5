4d48e0bfa0d3b1346ac45a9efaf3e53fc047ad49
Phurinon <folk.5834@gmail.com>
2024-10-07 21:41:42 +0700

    frontend testing

---
+++ testing/cypress/e2e/frontend/user/login.cy.ts
@@ -0,0 +1,14 @@
+describe("login test", () => {
+  it("login", () => {
+    cy.visit("http://localhost:5899/login");
+    // cy.get(
+    //   "#root > div > nav > div > div.nav-actions > div > a.nav-link.login"
+    // ).click();
+    cy.url().should("include", "/login");
+    cy.wait(2000);
+    cy.get("#email").type("aaa@gmail.com");
+    cy.get("#password").type("12345678");
+    cy.get('[data-cy="login-button"]').click();
+    cy.url().should("include", "/");
+  });
+});
