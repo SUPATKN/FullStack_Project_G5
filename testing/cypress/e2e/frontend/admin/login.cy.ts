1efe8a650c8d916784385e639b70b1b1f43a3cd7
Phurinon <folk.5834@gmail.com>
2024-10-11 10:02:21 +0700

    frontend testing

--- testing/cypress/e2e/frontend/admin/login.cy.ts
+++ testing/cypress/e2e/frontend/admin/login.cy.ts
@@ -1,5 +1,6 @@
 describe("admin login test", () => {
   it("login", () => {
+    cy.viewport("macbook-16");
     cy.visit("http://localhost:5899/login");
     // cy.get(
     //   "#root > div > nav > div > div.nav-actions > div > a.nav-link.login"
